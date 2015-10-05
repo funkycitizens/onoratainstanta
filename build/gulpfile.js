dae38236403c1aa2a7aef746b3625287aee2cd40
Alex Morega <alex@grep.ro>
2015-10-05 19:23:55 +0300

    include contact data

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -7,29 +7,43 @@ function template(name) {
   return Handlebars.compile(fs.readFileSync(name, {encoding: 'utf-8'}))
 }
 
-function instanta(row, folder) {
+function instanta(row, folder, contact) {
   var m = row.cod.match(/^(Judecatoria|Tribunalul|CurteadeApel)(.*)$/)
   var filename = '../_instante/' + folder + '/' + m[2].toLowerCase() + '.html'
   var html = template('instanta.html')({
     meta: row,
     indici_2013: row,
+    contact: contact,
   })
   fs.writeFileSync(filename, html)
 }
 
-function table(name) {
+function table(name, skip1) {
   var data = fs.readFileSync('tables/' + name + '.tsv', 'utf-8')
-  return d3.tsv.parse(data).slice(1)
+  return d3.tsv.parse(data)
+}
+
+function contacts(name) {
+  var rv = {}
+  table(name + '-contact').forEach(function(row) {
+    rv[row.cod] = row
+  })
+  return rv
 }
 
 gulp.task('instante', function() {
-  table('judecatorii-2013').forEach(function(row) {
-    instanta(row, 'judecatorii')
+  var contact = {
+    judecatorii: contacts('judecatorii'),
+    tribunale: contacts('tribunale'),
+    curtideapel: contacts('curtideapel'),
+  }
+  table('judecatorii-2013').slice(1).forEach(function(row) {
+    instanta(row, 'judecatorii', contact.judecatorii[row.cod])
   })
-  table('tribunale-2013').forEach(function(row) {
-    instanta(row, 'tribunale')
+  table('tribunale-2013').slice(1).forEach(function(row) {
+    instanta(row, 'tribunale', contact.tribunale[row.cod])
   })
-  table('curtideapel-2013').forEach(function(row) {
-    instanta(row, 'curtideapel')
+  table('curtideapel-2013').slice(1).forEach(function(row) {
+    instanta(row, 'curtideapel', contact.curtideapel[row.cod])
   })
 })
